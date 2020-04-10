fc78fdbaedb8fdc43f5860621bd865a4ce46d3eb
SailentzZz <mishatravin99@gmail.com>
2020-04-10 17:17:08 +0300

    first commit

---
+++ Assets/AnimatorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorScript : MonoBehaviour {
+
+	// Use this for initialization
+	Animator anim;
+	
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			anim.SetTrigger("JumpTrigger");
+		}
+	}
+}
